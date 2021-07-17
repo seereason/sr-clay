a20003f11fbc3cf8690be525b18c7c66835a90f8
David Fox <dsf@seereason.com>
2021-07-17 15:02:06 -0700

    * Version 1.2 * Make CssClass a superclass of CssStyle * Add class_ :: CssClass c => c -> Attribute

--- SeeReason/Css.hs
+++ SeeReason/Css.hs
@@ -3,10 +3,14 @@
 module SeeReason.Css
   ( CssClass(cssClass)
   , withHash
+  , protectCSSIdentifier
 #if SERVER
   , CssStyle(cssStyle)
   , byClass'
   , reifyCss
+#endif
+#if CLIENT
+  , _class
 #endif
   ) where
 
@@ -14,7 +18,7 @@ import Data.ByteString.Lazy as Lazy (fromStrict)
 import Data.Char (isAlphaNum, isAscii, isSpace)
 import Data.Digest.Pure.MD5 (md5)
 import Data.Serialize (encode)
-import Data.Text as Text (map, pack, singleton, Text)
+import Data.Text as Text ({-map,-} pack, singleton, Text)
 import qualified Data.Text as Text (concatMap)
 import GHC.Stack
 import GHC.Stack.Types
@@ -24,6 +28,9 @@ import Data.Default (Default(def))
 import Language.Haskell.TH (listE, litE, stringL, Type(AppT, ConT))
 import Language.Haskell.TH.Syntax (Dec(..), Exp, mkName, Q, reifyInstances, Type(VarT))
 import System.Directory ()
+#endif
+#if CLIENT
+import Alderon.Alderon  (Attribute, classes_)
 #endif
 
 -- | Instances of 'CssClass' can be converted to css class names.
@@ -40,8 +47,8 @@ protectCSSIdentifier t = Text.concatMap protectCSSChar t
     protectCSSChar '.' = "_" -- the backslash escaping doesn't seem to work
     protectCSSChar c | isAlphaNum c = singleton c
     protectCSSChar c | not (isAscii c) = singleton c
-    protectCSSChar c@'\n' = "_" -- hexEscape c
-    protectCSSChar c@'\r' = "_" -- hexEscape c
+    protectCSSChar '\n' = "_" -- hexEscape c
+    protectCSSChar '\r' = "_" -- hexEscape c
     protectCSSChar c | isSpace c = "_" -- hexEscape c
     protectCSSChar c = "\\" <> singleton c
 
@@ -57,7 +64,7 @@ withHash s = do
     locHash (PushCallStack _fn _loc more) = locHash more
     locHash (FreezeCallStack more) = locHash more
     locHash EmptyCallStack = ""
-
+#if 0
     protect :: Text -> Text
     protect = Text.map protectChar
 
@@ -68,18 +75,11 @@ withHash s = do
     protectChar '.' = '_'
     protectChar c | isSpace c = '_'
     protectChar c = '_'
-
--- data TestHash = TestHash deriving Show
---
--- instance CssClass TestHash where
---   cssClass = withHash . pack . show
---
--- > cssClass TestHash
--- "TestHash_84f295f"
+#endif
 
 #if SERVER
 -- | Instances of 'CssStyle' generate a Css value.
-class Default prefs => CssStyle a prefs | a -> prefs where
+class (CssClass a, Default prefs) => CssStyle a prefs | a -> prefs where
   cssStyle :: prefs -> Css
 
 byClass' :: CssClass a => a -> Refinement
@@ -94,4 +94,9 @@ reifyCss = do
   listE (concatMap (\case InstanceD _ _cxt (AppT (AppT _cls typ@(ConT tname)) prefs) _decs ->
                             [ [|($(litE (stringL (show tname))), cssStyle @ $(pure typ) @ $(pure prefs) def)|] ]
                           _ -> []) insts)
+#endif
+
+#if CLIENT
+class_ :: CssClass c => c -> Attribute
+class_ c = classes_ [cssClass c]
 #endif
